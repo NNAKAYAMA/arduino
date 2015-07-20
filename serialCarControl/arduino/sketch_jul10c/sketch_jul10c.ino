3129d4d11cc460b4a8649fbb05ad234ca8d9196c
natoto nakayama <nn67101460@gmail.com>
2015-07-20 12:49:38 +0900

    inoファイルの変更
    一つのループ内で2つシリアルから読み取るのは不安定になりそうなので修正
    ビット演算子を使い5bitめが01かでHIGH,LOWを判断

--- serialCarControl/arduino/sketch_jul10c/sketch_jul10c.ino
+++ serialCarControl/arduino/sketch_jul10c/sketch_jul10c.ino
@@ -1,36 +1,18 @@
-int BACK = 6,
-    FOWARD = 7,
-    LEFT = 9,
-    RIGHT = 10;
-int beta,
-    gamma;
-void setup() {
-  pinMode(BACK, OUTPUT);
-  pinMode(FOWARD, OUTPUT);
-  pinMode(LEFT, OUTPUT);
-  pinMode(RIGHT, OUTPUT);
+void setup(){
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
   Serial.begin(115200);
-}
+  }
 
 void loop() {
-  digitalWrite(FOWARD,LOW);
-  digitalWrite(BACK,LOW);
-  digitalWrite(LEFT,LOW);
-  digitalWrite(RIGHT,LOW);
-
-  if(Serial.available() > 2){ 
-    beta = Serial.read();
-    gamma = Serial.read();
-    Serial.write(beta); 
-    if(beta > 20 )
-      digitalWrite(BACK,HIGH);
-    if(beta < -20)
-      digitalWrite(FOWARD,HIGH);
-    if(gamma > 20)
-      digitalWrite(LEFT,HIGH);
-    if(gamma < -20)
-      digitalWrite(RIGHT,HIGH);
-     Serial.flush();
-  }
-}
+ if(Serial.available() > 0){
+  int pinNum = Serial.read();
+  if(pinNum & 16)
+    digitalWrite(pinNum - 16,HIGH);
+  else
+    digitalWrite(pinNum,LOW); 
+  }     
+}   
 
