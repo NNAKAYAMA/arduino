bed7c342ab5d4db3b57e39b77224b1d722d46b42
natoto nakayama <nn67101460@gmail.com>
2015-07-22 16:51:26 +0900

    server.jsの修正
    eventとデータの送信を分離
    送るデータをchar型1byteにしてみた

--- serialCarControl/server.js
+++ serialCarControl/server.js
@@ -15,20 +15,24 @@ http.listen(8000);
 /*
 ** シリアルポートの設定
 */
-var SerialPort = require("serialport").SerialPort
+var serialPort = require("serialport")
 var portName = 'COM3'; 
-var sp = new SerialPort(portName, {
+var sp = new serialPort.SerialPort(portName, {
   baudRate: 115200,
     dataBits: 8,
     parity: 'none',
-    stopBits: 1,
-    flowControl: true
+    stopBits: false,
+    flowControl: false
+    //parser: serialport.parsers.readline("\n")
 });
 
-int BACK = 6,
-    FOWARD = 7,
+var BACK = 6,
+    FOWARD = 0,
     LEFT = 9,
     RIGHT = 10;
+
+var BETA = 0,
+    GAMMA = 0;
 
 /*
 **アクセスが起こった時の反応
@@ -37,35 +41,33 @@ io.on('connection', function(socket) {
   console.log("connected");
   socket.on('tilt', function(tilt) {
     //console.log(tilt);
-    //arduinoに傾きの値を送信
-    // sp.write(tilt.beta);
-    // sp.write(tilt.gamma);
-
-    if(tilt.beta > 20)
-       sp.write(BACK + 16);
-    else
-      sp.write(BACK); 
+    BETA = tilt.beta;
+    GAMMA = tilt.gamma;
     
-    if(tilt.beta < -20)
-       sp.write(FOWARD + 16);
-    else
-      sp.write(FOWARD); 
-    
-    if(tilt.gamma > 20)
-      sp.write(LEFT + 16);
-    else
-      sp.wrtie(LEFT);
-
-    if(tilt.gamma < -20)
-      sp.write(RIGHT + 16):
-    else
-      sp.write(RIGHT);
     //すべてのクライアントと傾きの値を共有
     //io.sockets.emit("mobileTiltUpdated",tilt);
   });
 });
 
+//arduinoに傾きの値を送信
+var send = setInterval(function() {
+  var buff;
+  if(BETA > 30)
+    buff = '1'
+  else if(BETA < -30)
+    buff = '2';
+  else
+    buff = '0';
+  if(GAMMA > 30)
+    buff += '1';
+  else if(GAMMA < -30)
+    buff += '2';
+  else
+    buff += '0';
+ sp.write(buff);
+  },100);
+
 //arduinoからデータが送れれた時
-sp.on('data', function(input){
-  //console.log(input);
+sp.on('data', function(input) {
+//  console.log(input + "rsv data!!");
 });
