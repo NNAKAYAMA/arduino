5cf203eb87807b2288a351addedcb18cf55b4b3f
natoto nakayama <nn67101460@gmail.com>
2015-07-22 16:56:19 +0900

    .inoの修正
    serial.availableは送られたデータを受け取り更新
    車の処理はserial.available外で行う

--- serialCarControl/arduino/sketch_jul10c/sketch_jul10c.ino
+++ serialCarControl/arduino/sketch_jul10c/sketch_jul10c.ino
@@ -1,18 +1,46 @@
-void setup(){
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+int BACK = 6,
+    FOWARD = 7,
+    LEFT = 9,
+    RIGHT = 10;
+char beta = '0',
+    gamma = '0';
+void setup() {
+  pinMode(BACK, OUTPUT);
+  pinMode(FOWARD, OUTPUT);
+  pinMode(LEFT, OUTPUT);
+  pinMode(RIGHT, OUTPUT);
   Serial.begin(115200);
-  }
+}
 
 void loop() {
- if(Serial.available() > 0){
-  int pinNum = Serial.read();
-  if(pinNum & 16)
-    digitalWrite(pinNum - 16,HIGH);
-  else
-    digitalWrite(pinNum,LOW); 
-  }     
-}   
+  
+  if(Serial.available() > 1){ 
+    beta = Serial.read();
+    gamma = Serial.read();
+  }
+    if(beta == '1'){
+      digitalWrite(BACK,HIGH);
+      digitalWrite(BACK,LOW);
+    }
+    if(beta == '2'){
+      digitalWrite(BACK,LOW);
+      digitalWrite(FOWARD,HIGH);
+    }
+    if(beta == '0'){
+      digitalWrite(BACK,LOW);
+      digitalWrite(FOWARD,LOW);
+    }
+    if(gamma == '1'){
+      digitalWrite(LEFT,HIGH);
+      digitalWrite(RIGHT,LOW);
+    }
+    if(gamma == '2'){
+      digitalWrite(LEFT,LOW);
+      digitalWrite(RIGHT,HIGH);
+    }
+    if(gamma == '0'){
+      digitalWrite(LEFT,LOW);
+      digitalWrite(RIGHT,LOW);
+    }
+}
 
